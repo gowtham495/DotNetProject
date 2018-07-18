e07019701acb14f8d2ca931383a1af4f5eb7de26
jegasuguna <jegasuguna.murugan@cognizant.com>
2018-07-18 12:22:32 +0530

    added

---
+++ add/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add
+{
+    
+        public class program
+        {
+            public int sum(int n1, int n2)
+            {
+                int c;
+                c = n1 + n2;
+                return c;
+            }
+            public static void Main(String[] args)
+            {
+                program d = new program();
+                Console.WriteLine("enter the number");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("enter the number");
+                int b = int.Parse(Console.ReadLine());
+
+
+
+
+                Console.WriteLine("the number is:{0}", d.sum(a, b));
+
+            }
+
+        }
+    }
+
---
+++ addTests/programTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using add;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add.Tests
+{
+    [TestClass()]
+    public class programTests
+    {
+        [TestMethod()]
+        public void sumTest()
+        {
+
+            program b = new program();
+
+            Assert.AreEqual(4, b.sum(2, 2));
+        }
+    }
+}
